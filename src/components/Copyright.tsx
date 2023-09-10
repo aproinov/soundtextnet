4f3d8564d2df37aefb152ae2e43b62baf25bbb0a
Sound Text <143975674+soundtext@users.noreply.github.com>
2023-09-10 19:25:47 +0700

    Update Copyright.tsx

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -6,7 +6,7 @@ export default function Copyright() {
         <a href="/about">About</a> &nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp; 
         <a href="/terms">Terms</a> &nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp; 
         <a href="/disclaimer">Disclaimer</a> &nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp; 
-        <a href="https://www.sebuahutas.com/sound-of-text/">Sebuahutas.com</a>
+        <a href="https://www.sebuahutas.com/sound-of-text/">Tools</a>
       </div><br />&copy; 2022 - Teknosia</p>
       <style jsx>
         {`
